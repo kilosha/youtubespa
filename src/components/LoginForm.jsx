6937b672a2700dfc3fb8ed9345ac9f431daffda9
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2024-12-24 22:12:57 +0300

    add react-hook-form and messages to Login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import { useNavigate } from 'react-router';
 
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Checkbox, Form, Input, message } from 'antd';
 import axios from 'axios';
+
+import { FormItem } from 'react-hook-form-antd';
+import { useForm } from 'react-hook-form';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as z from 'zod';
 
 const LoginForm = () => {
     let navigate = useNavigate();
+    const [messageApi, contextHolder] = message.useMessage();
+
+    // const schema = z.object({
+    //     username: z
+    //         .string()
+    //         .min(1, { message: 'Required' })
+    //         .max(15, { message: 'Username should be less than 15 characters' }),
+    //     password: z.string().min(1, { message: 'Required' }),
+    //     remember: z.boolean()
+    //});
+
+    const { control, handleSubmit } = useForm({
+        // defaultValues: { username: '', password: '' }
+        // resolver: zodResolver(schema)
+    });
+
+    const openMessage = (type, message) => {
+        messageApi.open({
+            key: 'my',
+            type,
+            content: message || type
+        });
+    };
 
     const onFinish = (values) => {
+        openMessage('loading');
         axios
             .post(`${import.meta.env.VITE_BACKEND_URL_DEV}/api/auth/login`, values)
             .then(function (response) {
@@ -15,67 +45,84 @@ const LoginForm = () => {
                 navigate('/');
             })
             .catch((e) => {
-                alert(e?.response?.data?.message);
+                openMessage('error', e?.response?.data?.message);
+                //alert(e?.response?.data?.message);
             });
-
-        //console.log('Success:', values);
     };
+
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
     return (
-        <Form
-            name="basic"
-            labelCol={{
-                span: 8
-            }}
-            wrapperCol={{
-                span: 16
-            }}
-            style={{
-                maxWidth: 600
-            }}
-            initialValues={{
-                remember: true
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off">
-            <Form.Item
-                label="Логин"
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Введите логин!'
-                    }
-                ]}>
-                <Input />
-            </Form.Item>
+        <>
+            {contextHolder}
 
-            <Form.Item
-                label="Пароль"
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Введите пароль"'
-                    }
-                ]}>
-                <Input.Password />
-            </Form.Item>
+            <Form
+                name="basic"
+                labelCol={{
+                    span: 8
+                }}
+                wrapperCol={{
+                    span: 16
+                }}
+                style={{
+                    maxWidth: 600
+                }}
+                initialValues={{
+                    remember: true
+                }}
+                onFinish={handleSubmit((data) => {
+                    onFinish(data);
+                })}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off">
+                <FormItem
+                    control={control}
+                    label="Логин"
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Введите логин'
+                        },
+                        {
+                            pattern: /^[a-zA-Z0-9_]+$/,
+                            message: 'Username should contain only letters, numbers and _'
+                        }
+                    ]}>
+                    <Input />
+                </FormItem>
 
-            {/* <Form.Item name="remember" valuePropName="checked" label={null}>
+                <FormItem
+                    control={control}
+                    label="Пароль"
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Введите пароль'
+                        },
+                        {
+                            pattern: /^[a-zA-Z0-9_]+$/,
+                            message:
+                                'Password should contain only letters, numbers and special signs like !@_-'
+                        }
+                    ]}>
+                    <Input.Password />
+                </FormItem>
+
+                {/* <Form.Item name="remember" valuePropName="checked" label={null}>
                 <Checkbox>Запомнить меня</Checkbox>
             </Form.Item> */}
 
-            <Form.Item label={null}>
-                <Button type="primary" htmlType="submit">
-                    Войти
-                </Button>
-            </Form.Item>
-        </Form>
+                <Form.Item label={null}>
+                    <Button type="primary" htmlType="submit">
+                        Войти
+                    </Button>
+                </Form.Item>
+            </Form>
+        </>
     );
 };
 
