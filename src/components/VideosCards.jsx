935a0ee93ca96e27a619ecf75c2d260816ffdc71
Maria Gutkovskaya <gutkovskaya18@gmail.com>
2024-12-24 22:13:24 +0300

    improve VideosCards layout

--- src/components/VideosCards.jsx
+++ src/components/VideosCards.jsx
@@ -1,4 +1,4 @@
-import { Card } from 'antd';
+import { Card, Col, Row } from 'antd';
 const { Meta } = Card;
 
 const VideosCards = ({ data }) => {
@@ -11,7 +11,58 @@ const VideosCards = ({ data }) => {
 
     return (
         <div>
-            <Card>
+            <Row gutter={16}>
+                {data.map((item) => {
+                    return (
+                        <Col span={5}>
+                             <Card cover={<img alt="example" src={item.picture} style={{width: '40px'}}/>}>
+                                <Meta
+                                    title={
+                                        <a
+                                            href={`https://www.youtube.com/watch?v=${item.id}`}
+                                            target="_blank">
+                                            {item.title}
+                                        </a>
+                                    }
+                                    description={item.description}
+                                />
+                            </Card>
+                        </Col>
+                        // <Card.Grid style={gridStyle} key={item.id}>
+                            // <Card cover={<img alt="example" src={item.picture} />}>
+                            //     <Meta
+                            //         title={
+                            //             <a
+                            //                 href={`https://www.youtube.com/watch?v=${item.id}`}
+                            //                 target="_blank">
+                            //                 {item.title}
+                            //             </a>
+                            //         }
+                            //         description={item.description}
+                            //     />
+                            // </Card>
+                        // </Card.Grid>
+                    );
+                })}
+
+                {/* <Col span={8}>
+                    <Card title="Card title" bordered={false} >
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col> */}
+            </Row>
+
+            {/* <Card>
                 {data.map((item) => {
                     return (
                         <Card.Grid style={gridStyle} key={item.id}>
@@ -30,7 +81,7 @@ const VideosCards = ({ data }) => {
                         </Card.Grid>
                     );
                 })}
-            </Card>
+            </Card> */}
         </div>
     );
 };
